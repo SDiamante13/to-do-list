52721b596de13e73393a9d7da8c6241b71d1cf03
Steven Diamante <steven@pivotal.io>
2019-10-13 19:55:21 -0500

    Add Todo list items and the checkbox and delete button logic.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  deleteTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
